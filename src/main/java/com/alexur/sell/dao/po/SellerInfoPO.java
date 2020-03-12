83404f013764925e0cee13750f34a0099dd67ea5
dg-dhg <630909931@qq.com>
2020-03-12 14:01:36 +0800

    Initial commit

---
+++ src/main/java/com/alexur/sell/dao/po/SellerInfoPO.java
@@ -0,0 +1,11 @@
+package com.alexur.sell.dao.po;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+public class SellerInfoPO{
+
+}
---
+++ src/test/java/com/alexur/sell/SellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexur.sell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellApplicationTests{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
